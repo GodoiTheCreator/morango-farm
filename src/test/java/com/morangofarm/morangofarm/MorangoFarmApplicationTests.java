612d4c25ae94bef422f85612cb1f090c64db849a
Eduardo Godoi da Silveira <eduardogodoisilveira@gmail.com>
2024-02-12 16:40:45 -0300

    Adding Spring Boot Initializer

---
+++ src/test/java/com/morangofarm/morangofarm/MorangoFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morangofarm.morangofarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorangoFarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
